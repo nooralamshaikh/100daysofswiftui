c2a91c2252a590d730dca172a5b2f00919d9383b
Nooralam Shaikh <shkhnooralam6@gmail.com>
2024-01-27 12:37:38 +0530

    day 1 - completed

---
+++ day1/variable_constants.playground/Contents.swift
@@ -0,0 +1,42 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+var name = "Nooralam"
+name = "Sufiya"
+
+let character = "hero"
+
+var playerName = "Roy"
+print(playerName)
+
+playerName = "Dani"
+print(playerName)
+
+playerName = "Sam"
+print(playerName)
+
+
+var multiLineString = """
+Hello, darkness my old friend
+where are you?
+"""
+
+let score = 10
+let reallyBigNumber = 100____00_00_00
+
+let lowerScore = score - 2
+let higherScore = score + 2
+let doubleScore = score * 2
+let squaredScore = score * score
+let halvedScore = score / 2
+
+var counter = 20
+counter += 5
+
+let number = 120
+number.isMultiple(of: -1)
+
+
+let numbera = 1.2 + 2.03
+print(numbera)
