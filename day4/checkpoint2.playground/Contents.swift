1f867c9c4cf1a9e14a8abf07ac5cff5f59056392
Nooralam Shaikh <shkhnooralam6@gmail.com>
2024-01-30 08:12:57 +0530

    day 4 completed

---
+++ day4/checkpoint2.playground/Contents.swift
@@ -0,0 +1,4 @@
+import UIKit
+
+let array = ["Mumbai", "Delhi", "Kolkata", "Mumbai"]
+print("Number of items in array: \(array.count), unique items: \(Set(array).count)")
---
+++ day4/type_annotation_inference.playground/Contents.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+// Type annotations let us be explicit about what data types we want, and look like this:
+let surname: String = "Lasso"
+
+// inference
+var score = 0
+
+let playerName: String = "Roy"
+var luckyNumber: Int = 13
+let pi: Double = 3.141
+var isAuthenticated: Bool = true
+var albums: [String] = ["Red", "Fearless"]
+var user: [String: String] = ["id": "@twostraws"]
+var books: Set<String> = Set(["The Bluest Eye", "Foundation", "Girl, Woman, Other"])
+var soda: [String] = ["Coke", "Pepsi", "Irn-Bru"]
+
+var teams: [String] = [String]()
+var cities: [String] = []
+var clues = [String]()
+
+enum UIStyle {
+    case light, dark, system
+}
+
+var style = UIStyle.light
+style = .dark
+
+let username: String
+// lots of complex logic
+username = "@twostraws"
+// lots more complex logic
+print(username)
+
+let x: Double = 112
