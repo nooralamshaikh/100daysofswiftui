a935c00d2fc61c8eed47ad38df2dad34235c6a98
Nooralam Shaikh <shkhnooralam6@gmail.com>
2024-02-04 20:46:18 +0530

    project 1 completed - day18

--- day16/WeSplit/WeSplit/ContentView.swift
+++ day16/WeSplit/WeSplit/ContentView.swift
@@ -8,18 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
-    let students = ["Harry", "Hermoine", "Ron"]
-    @State private var selectedStudent = ""
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var isAmountFocused
+
+    private var totalPerPerson: Double {
+        let numberOfPeople = Double(numberOfPeople + 2)
+        let perPersonAmount = grandTotal / numberOfPeople
+        return perPersonAmount
+    }
+
+    private var grandTotal: Double {
+        let tipSelection = Double(tipPercentage)
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        return grandTotal
+    }
+
     var body: some View {
         NavigationStack {
             Form {
-                Picker("Select your student", selection: $selectedStudent) {
-                    ForEach(students, id: \.self) {
-                        Text($0)
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "INR"))
+                        .keyboardType(.decimalPad)
+                        .focused($isAmountFocused)
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                Section("How much do you want to tip?") {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0...100, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }.pickerStyle(.navigationLink)
+                }
+                Section("Amount per person") {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "INR"))
+                }
+                Section("Grand total amount") {
+                    Text(grandTotal, format: .currency(code: Locale.current.currency?.identifier ?? "INR"))
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                if isAmountFocused {
+                    Button("Done") {
+                        isAmountFocused = false
                     }
                 }
             }
-            .navigationTitle("Select student")
         }
     }
 }
